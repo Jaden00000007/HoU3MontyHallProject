7cfbb5559357c11b62429f5f03906d9d8d71eaf9
Jaden00000007 <146095161+Jaden00000007@users.noreply.github.com>
2023-11-08 08:41:12 -0500

    Update MontyHall.java

--- src/MontyHall.java
+++ src/MontyHall.java
@@ -1,50 +1,69 @@
 public class MontyHall {
 
-    private int goat1;
-    private int goat2;
-    private int car;
+  private int goat1;
+  private int goat2;
+  private int car;
 
-    public void setUp(){
-        car = (int)(Math.random()*3)+1;
-        if (car == 1){
-            goat1 = 2;
-            goat2 = 3;
-        }
-        else if (car == 2){
-            goat1 = 1;
-            goat2 = 3;
-        }
-        else {
-            goat1 = 1;
-            goat2 = 2;
-        }
+  public void setUp(){
+    car = (int)(Math.random()*3)+1;
+    if (car == 1){
+      goat1 = 2;
+      goat2 = 3;
     }
-
-    public String reveal(int user){
-        if (user == goat2) {
-            return "One of the goats is behind door number " + goat1;
-        }
-        else if (user == goat1){
-            return "One of the goats is behind door number " + goat2;
-        }
-        else{
-            return "One of the goats is behind door number " + goat1;
-        }
+    else if (car == 2){
+      goat1 = 1;
+      goat2 = 3;
     }
-
-    public boolean Guessed(int user){
-        if (user == car){
-            return true;
-        }
-        else{
-            return false;
-        }
+    else {
+      goat1 = 1;
+      goat2 = 2;
     }
+  }
 
-    public boolean Switch(String user,int user1){
-        if (user.equals("Yes")){
-
-        }
+  public int reveal(int user){
+    if (user == goat2) {
+      int revealed = goat1;
+      return revealed;
     }
+    else if (user == goat1){
+      int revealed = goat2;
+      return revealed;
+    }
+    else{
+      int revealed = goat1;
+      return revealed;
+    }
+  }
 
+  public int Switch(int user1, int revealed){
+    int switched = 0;
+      if (user1 == 1 && revealed == 2){
+        switched = 3;
+      }
+      else if (user1 == 1 && revealed == 3){
+        switched = 2;
+      }
+      else if (user1 == 2 && revealed == 1){
+        switched = 3;
+      }
+      else if (user1 == 2 && revealed == 3){
+        switched =1;
+      }
+      else if (user1 == 3 && revealed == 2){
+        switched = 1;
+      }
+      else if (user1 == 3 && revealed == 1){
+        switched = 2;
+      } 
+      return switched;
+    } 
+
+  public void Guessed(int user1){
+    if (user1 == car){
+      System.out.print("You won!");
+    }
+    else if (user1!= car){
+      System.out.print("The car is behind door " + car + ". You lost! (Or you could say you won a goat!)");
+    }
+  }
 }
